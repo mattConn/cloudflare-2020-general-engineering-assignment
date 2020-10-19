84dde21c2dd65701198fafb14d8dd89a08fb209f
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-10-19 02:46:01 -0400

    Returning link array json only at "/links" uri.

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
+// make link object
+const makeLink = (name,url) => {
+    return {
+        name: name,
+        url: url
+    };
+};
+
+const links = [
+    makeLink('Go by Example','https://gobyexample.com/'),
+    makeLink('Google', 'https://www.google.com/'),
+    makeLink('GitHub', 'https://github.com/'),
+    makeLink('Foobiebletch', 'http://foobiebletch.net/'),
+    makeLink('Cloudflare Workers', 'https://workers.cloudflare.com/'),
+];
+
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+
+	const uri = request.url.split('/').pop();
+	let content;
+	let headers = {};
+
+	switch(uri){
+		case 'links': // respond with json at /links uri
+			content = JSON.stringify(links);
+    		headers = { 'content-type': 'application/json' };
+		break;
+
+		default:
+			content = uri; // respond with uri
+    		headers = { 'content-type': 'text/html' };
+	}
+
+  	return new Response(content, { headers: headers });
 }
