7894be4c4afb8bbeba2607ad210b11fc48948f9f
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-10-20 03:51:49 -0400

    writing links to static html with HTMLRewriter

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ class Link {
 	}
 }
 
+// for writing links to html
+class LinksTransformer {
+  constructor(links) {
+    this.links = links;
+  }
+  
+  async element(element) {
+	element.setInnerContent(
+		this.links.map(
+			link => `<a href="${link.url}">${link.name}</a>`
+		).join(''), {html: true});
+  }
+}
+
 const links = [
     new Link('Go by Example','https://gobyexample.com/'),
     new Link('Google', 'https://www.google.com/'),
@@ -33,12 +47,12 @@ async function handleRequest(request) {
 		break;
 
 		default: // render static html
-			const url = 'https://static-links-page.signalnerve.workers.dev/';
-			body = await fetch(url) // html
-				.then(response => response.text())
-				.then(HTML => HTML);
 
-    		headers = { 'content-type': 'text/html' };
+			const url = 'https://static-links-page.signalnerve.workers.dev/';
+
+			const response = await fetch(url);
+			const targetElement = 'div#links';
+			return new HTMLRewriter().on(targetElement, new LinksTransformer(links)).transform(response);
 	}
 
   	return new Response(body, { headers: headers });
